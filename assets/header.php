c48412827aa18ed762466176936b3d4d9a90794c
VICTORJOSEARAVENALANDE <36843312+VICTORJOSEARAVENALANDE@users.noreply.github.com>
2018-04-06 14:44:51 +0200

    Gryffindor og innlogging greier
    Fikset style til gryffindor og start på innlogging

--- assets/header.php
+++ assets/header.php
@@ -1,61 +1,61 @@
 <header>
     <div class="header-image">
     </div>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Hogwarts </a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="index.php">Hogwarts </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-    <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Gryffindor</a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">House</a>
-          <a class="dropdown-item" href="#">Students</a>
-          <a class="dropdown-item" href="#">Points</a>
-        </div>
-      </li>
-    
-    <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Ravenclaw
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">House</a>
-          <a class="dropdown-item" href="#">Students</a>
-          <a class="dropdown-item" href="#">Points</a>
-        </div>
-      </li>
-        
-    <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Slytherin
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">House</a>
-          <a class="dropdown-item" href="#">Students</a>
-          <a class="dropdown-item" href="#">Points</a>
-        </div>
-      </li>
-        
-    <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Hufflepuff
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">House</a>
-          <a class="dropdown-item" href="#">Students</a>
-          <a class="dropdown-item" href="#">Points</a>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">School rules</a>
-        </li>
-        <li class="nav-item">
-        <a class="nav-link" href="#">Quidditch</a>
-      </li>
-      </div>
-</nav>
-</header>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Gryffindor</a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="gryffindor.php">House</a>
+                        <a class="dropdown-item" href="#">Students</a>
+                        <a class="dropdown-item" href="#">Points</a>
+                    </div>
+                </li>
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Ravenclaw
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">House</a>
+                        <a class="dropdown-item" href="#">Students</a>
+                        <a class="dropdown-item" href="#">Points</a>
+                    </div>
+                </li>
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Slytherin
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">House</a>
+                        <a class="dropdown-item" href="#">Students</a>
+                        <a class="dropdown-item" href="#">Points</a>
+                    </div>
+                </li>
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Hufflepuff
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">House</a>
+                        <a class="dropdown-item" href="#">Students</a>
+                        <a class="dropdown-item" href="#">Points</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">School rules</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Quidditch</a>
+                </li>
+                </div>
+            </nav>
+        </header>

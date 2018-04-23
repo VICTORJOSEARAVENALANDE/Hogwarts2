609e0fa06e78d469676284b3aac623812845f300
VICTORJOSEARAVENALANDE <36843312+VICTORJOSEARAVENALANDE@users.noreply.github.com>
2018-04-23 13:53:47 +0200

    Kult

--- assets/header.php
+++ assets/header.php
@@ -28,7 +28,7 @@
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         Slytherin
-                    </a>s
+                    </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                         <a class="dropdown-item" href="slytherin.php">House</a>
                     </div>
